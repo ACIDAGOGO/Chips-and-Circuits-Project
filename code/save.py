f215ee495f2f0b83bd3bc040d2a7798359d708b6
ACIDAGOGO <casperleenaars@hotmail.com>
2023-06-19 16:54:37 +0200

    added save script which writes important output to a csv file

--- code/save.py
+++ code/save.py
@@ -1,12 +1,28 @@
-import json
+import csv
 import sys
 sys.path.append("classes")
 
 from chip import Chip
 
-def save_to_file(chip: 'Chip', output_filename: str):
-    # to_json(chip.grid)
-    json_data = json.dumps(chip.__dict__, skipkeys=True)
+def extract_data(chip: 'Chip') -> list[int]:
+    data: list = []
 
-    with open(f'../output/{output_filename}.json', "a") as file:
-        file.write(json_data + "\n")
+    # Adds iteration, cost, wirecount, intersectioncount to data list
+    data.append(chip.iteration)
+    data.append(chip.cost)
+    data.append(chip.wirecount)
+    data.append(chip.intersectioncount)
+
+    return data
+
+
+def save_to_file(chip: 'Chip', output_filename: str):
+    data = extract_data(chip)
+
+    # Opens output file and appends latest chip iteration data
+    with open(f'../output/{output_filename}.csv', "a", newline = "") as file:
+        writer = csv.writer(file)
+        if data[0] == 0:
+            writer.writerow(["iteration","cost","wirecount","intersectioncount"])
+
+        writer.writerow(data)
